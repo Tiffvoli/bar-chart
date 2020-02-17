72afb6790e263f4eee25e0f52afb4e86449854af
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-02-17 11:07:59 +0100

    fixed codes

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 "use strict";
-//window.addEventListener("DOMContentLoaded", getNumberOfCustomers);
+window.addEventListener("DOMContentLoaded", start);
+
+const num = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let bar;
+let numofbar;
+
+function start() {
+  if (num.length < 20) num.push(getNumberOfCustomers());
+  else {
+    num.shift();
+    num.push(getNumberOfCustomers());
+  }
+  setHeight();
+}
+
+function setHeight() {
+  for (let i = 0; i < num.length; i++) {
+    bar = document.querySelector(`#bars > div:nth-child(${i + 1})`);
+    bar.style.setProperty("--height", num[i]);
+    numofbar = document.querySelector(`#nums > div:nth-child(${i + 1})`);
+    numofbar.textContent = num[i];
+  }
+  document.querySelectorAll("#nums > div").forEach(
+    (numofbar, i) =>
+      function() {
+        numofbar.innerHTML = num[i];
+      }
+  );
+
+  setTimeout(start, 1000);
+}
 
 function getNumberOfCustomers() {
   return Math.floor(Math.random() * 32);
 }
-
-const queueSize = getNumberOfCustomers();
-console.log(queueSize);
-function setHeight() {
-  document.querySelectorAll(".bar").forEach((bar, i) => {
-    bar.style.setProperty("--height", queueSize[i]);
-  });
-
-  for (let i = 0; i < queueSize.length; i++) {
-    let bar;
-
-    bar = document.querySelector(`#container > div:nth-child(${i + 1})`);
-    bar.style.setProperty("--height", queueSize[i]);
-  }
-  console.log(queueSize);
-  setTimeout(setHeight, 600);
-}
-
-setHeight();
